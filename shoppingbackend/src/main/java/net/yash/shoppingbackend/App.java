4a83f389b40ccef07100d9881d131b42a15ff769
YASHWANTH <77yashwanth@gmail.com>
2018-10-20 23:34:53 +0530

    Project Setup Has SuccessFully Completed

---
+++ shoppingbackend/src/main/java/net/yash/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.yash.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
