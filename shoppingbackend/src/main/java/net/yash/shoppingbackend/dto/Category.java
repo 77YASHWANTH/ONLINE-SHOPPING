d9d65e0c7d331f89b765f14995be15d7c21bb832
YASHWANTH <77yashwanth@gmail.com>
2018-11-04 21:28:23 +0530

    Project Updated SuccessFully

---
+++ shoppingbackend/src/main/java/net/yash/shoppingbackend/dto/Category.java
@@ -0,0 +1,46 @@
+package net.yash.shoppingbackend.dto;
+
+public class Category {
+	
+	/* PRIVATE PARTS*/
+	
+	private int id;
+	private String description;
+	private String name;
+	private String imageUrl;
+	private boolean active = true;
+
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	public boolean isActive() {
+		return active;
+	}
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	
+}
